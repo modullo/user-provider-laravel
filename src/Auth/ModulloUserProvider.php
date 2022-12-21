4f303cea41d427ab793c939b5cc72c123a700277
Damilare Abiodun <dami@20ksites.com>
2022-12-21 14:36:36 +0100

    Troubleshooting hostville login

--- src/Auth/ModulloUserProvider.php
+++ src/Auth/ModulloUserProvider.php
@@ -130,6 +130,7 @@ class ModulloUserProvider implements UserProvider
   public function retrieveByCredentials(array $credentials, string $module = null )
   {
     $token = login_via_password($this->sdk, $credentials['email'] ?? '', $credentials['password'] ?? '');
+    dd($token);
     # we get the authentication token
     if ($token instanceof ModulloResponse) {
       return null;
